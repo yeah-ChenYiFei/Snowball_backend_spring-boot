1a5fb4a3072d1495b63037c63fa2d7656ce538ba
quanty6225 <2502013337@qq.com>
2026-05-31 01:33:00 +0800

    v1.0.2，增加了英雄页功能

--- src/main/java/com/snowball/config/RedisConfig.java
+++ src/main/java/com/snowball/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.snowball.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,6 +11,11 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
